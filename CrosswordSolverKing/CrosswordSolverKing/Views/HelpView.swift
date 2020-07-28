2ea459d0a79048eeb72608dd92e8adbfe7f04903
Mark Bailey <markpdbailey@gmail.com>
2020-07-28 12:57:32 +0100

    Added example and more info sections on the help tip

--- CrosswordSolverKing/CrosswordSolverKing/Views/HelpView.swift
+++ CrosswordSolverKing/CrosswordSolverKing/Views/HelpView.swift
@@ -19,16 +19,48 @@ struct HelpView: View {
                     .font(Font.system(.largeTitle))
                     .foregroundColor(Color.yellow)
                     .padding(8)
-                Text(viewModel.tip.title).font(.largeTitle)
+                Text(viewModel.tip.title)
+                    .font(.largeTitle)
                 Spacer()
             }
             Text(viewModel.tip.description)
+                .padding(8)
+            VStack(alignment: .leading) {
+                Text("Example")
+                    .font(.headline)
+                    .underline()
+                    .padding(.top, 8)
+                    .padding(.bottom, 8)
+                Text("Try ")+Text(viewModel.tip.showMe).foregroundColor(.blue) + Text(" to find:")
+                Text(viewModel.tip.example)
+                    .foregroundColor(.red)
+                    .padding(.top, 8)
+                    .padding(.bottom, 8)
+                HStack {
+                    Spacer()
+                    Button(action: viewModel.showMe){
+                        Text("Show Me")
+                    }
+                }
+            }
+            .padding(8)
+            
+            VStack(alignment: .leading) {
+                Text("More Info")
+                    .font(.headline)
+                    .underline()
+                    .padding(.top, 8)
+                    .padding(.bottom, 8)
+
+                Text(viewModel.tip.advanced)
+                    .font(.body)
+            }
+            .padding(8)
         }
         .onDisappear{
             self.viewModel.onDisappear()
         }
         .navigationBarTitle(Text("Help"), displayMode: .inline)
-        .navigationBarItems(trailing: Button(action: viewModel.showMe){ Text("Show Me")})
     }
 }
 
