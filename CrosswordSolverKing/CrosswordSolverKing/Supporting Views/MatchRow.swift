6abaa910c264729a9c3ae1bd0cba50dab5ca5c93
Mark Bailey <markpdbailey@gmail.com>
2020-08-05 15:50:24 +0100

    Match row will display a toast when copy button pressed

--- CrosswordSolverKing/CrosswordSolverKing/Supporting Views/MatchRow.swift
+++ CrosswordSolverKing/CrosswordSolverKing/Supporting Views/MatchRow.swift
@@ -12,6 +12,7 @@ import SwiftUtils
 struct MatchRow: View {
     @State private var speakIconScale : CGFloat = 1
     @State private var copyIconScale : CGFloat = 1
+    @State private var isCopiedVisible = false
     let match : String
     let formatter : WordFormatter
     
@@ -27,29 +28,44 @@ struct MatchRow: View {
     private func copy(){
         UIPasteboard.general.string = match
         UINotificationFeedbackGenerator().notificationOccurred(.success)
+        isCopiedVisible = true
         copyIconScale = 2
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
             self.copyIconScale = 1
         }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.$isCopiedVisible.animation(.linear).wrappedValue = false
+        }
     }
     
     var body: some View {
-        HStack {
-            Text(formatter.format(match))
-            Spacer()
-            Image(systemName: "speaker.3")
-                .foregroundColor(Color.blue)
-                .padding(.trailing,8)
-                .onTapGesture(perform: speak)
-                .scaleEffect(speakIconScale)
-                .animation(Animation.easeOut.speed(2))
-            Image(systemName: "doc.on.doc")
-                .foregroundColor(Color.blue)
-                .padding(.trailing,8)
-                .onTapGesture(perform: copy)
-                .scaleEffect(copyIconScale)
-                .animation(Animation.linear.speed(4))
-        }.padding(8)
+        ZStack {
+            HStack {
+                Text(formatter.format(match))
+                Spacer()
+                Image(systemName: "speaker.3")
+                    .foregroundColor(Color.blue)
+                    .padding(.trailing,8)
+                    .onTapGesture(perform: speak)
+                    .scaleEffect(speakIconScale)
+                    .animation(Animation.easeOut.speed(2))
+                Image(systemName: "doc.on.doc")
+                    .foregroundColor(Color.blue)
+                    .padding(.trailing,8)
+                    .onTapGesture(perform: copy)
+                    .scaleEffect(copyIconScale)
+                    .animation(Animation.linear.speed(4))
+            }.padding(8)
+            if isCopiedVisible {
+                Text("Copied")
+                    .foregroundColor(Color.white)
+                    .padding(.leading, 12)
+                    .padding(.trailing, 12)
+                    .padding(.top,6)
+                    .padding(.bottom,6)
+                    .background(Capsule().fill(Color.blue))
+            }
+        }
     }
 }
 
